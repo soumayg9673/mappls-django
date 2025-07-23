14a5caca848955f4cbb1d7a3127f0b5485eeb083
soumayg9673 <pmrs9673@gmail.com>
2025-07-23 14:34:33 +0530

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mappls-map-widget",
-    version="0.1.0",
+    version="0.1.1",
     description="Mappls Map Widget for Django",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
