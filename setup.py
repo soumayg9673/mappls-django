f8749d4d1f45877ba2363aec6fb8f3eee2cbcbf3
soumayg9673 <pmrs9673@gmail.com>
2025-07-23 13:00:12 +0530

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mappls-django",
+    version="0.1.0",
+    description="Mappls Map Widget for Django",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Soumay Gupta",
+    author_email="pmrs9673@gmail.com",
+    url="https://github.com/soumayg9673/mappls-django",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Django>=3.2",
+    ],
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
